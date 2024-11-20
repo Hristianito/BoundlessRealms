dce6ce3f03d49a92e82aa3ce1a1be565380cfd70
Hristianito <hristotk3475@gmail.com>
2024-11-20 17:07:10 +0200

    + Box Weapon Tracing c++

--- Source/BoundlessRealms/Private/Items/Weapons/Weapon.cpp
+++ Source/BoundlessRealms/Private/Items/Weapons/Weapon.cpp
@@ -4,7 +4,32 @@
 #include "Items/Weapons/Weapon.h"
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/SceneComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "MainCharacter.h"
+
+AWeapon::AWeapon()
+{
+	WeaponBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Weapon Box"));
+	WeaponBox->SetupAttachment(GetRootComponent());
+	WeaponBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	WeaponBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	WeaponBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	BoxTraceStart = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace Start"));
+	BoxTraceStart->SetupAttachment(GetRootComponent());
+
+	BoxTraceEnd = CreateDefaultSubobject<USceneComponent>(TEXT("Box Trace End"));
+	BoxTraceEnd->SetupAttachment(GetRootComponent());
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+
+	WeaponBox->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnBoxOverlap);
+}
 
 void AWeapon::Equip(USceneComponent* InParent, FName InSocketName)
 {
@@ -34,4 +59,27 @@ void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* 
 void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
 	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
+
+void AWeapon::OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FVector WeaponStart = BoxTraceStart->GetComponentLocation();
+	const FVector WeaponEnd = BoxTraceEnd->GetComponentLocation();
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+	FHitResult HitResult;
+
+	UKismetSystemLibrary::BoxTraceSingle(
+		this,
+		WeaponStart,
+		WeaponEnd,
+		FVector(5.f, 5.f, 5.f),
+		BoxTraceStart->GetComponentRotation(),
+		ETraceTypeQuery::TraceTypeQuery1,
+		false,
+		ActorsToIgnore,
+		EDrawDebugTrace::ForDuration,
+		HitResult,
+		true
+	);
 }
--- Source/BoundlessRealms/Public/Items/Weapons/Weapon.h
+++ Source/BoundlessRealms/Public/Items/Weapons/Weapon.h
@@ -7,6 +7,8 @@
 #include "Weapon.generated.h"
 
 class USoundBase;
+class UBoxComponent;
+class USceneComponent;
 
 /**
  * 
@@ -18,18 +20,34 @@ class BOUNDLESSREALMS_API AWeapon : public AItem
 
 public:
 
+	AWeapon();
+
 	void Equip(USceneComponent* InParent, FName InSocketName);
 
 	void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
 
 protected:
 	
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION()
+	void OnBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
 	
 	UPROPERTY(EditAnywhere, Category = "Weapon Properties")
 	USoundBase* FirstEquipSound; 
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	UBoxComponent* WeaponBox;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USceneComponent* BoxTraceStart;
+
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	USceneComponent* BoxTraceEnd;
 };
